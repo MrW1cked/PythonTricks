07a64b99a23e1049219c0c7eef50996ae06620e0
MrW1cked <nunofvsousa@gmail.com>
2022-10-25 21:21:59 +0100

    Create String_Reverse.py
    Reverses a String and Capitalises the first letter of each word in the string

---
+++ Classes/String_Reverse.py
@@ -0,0 +1,9 @@
+def func(x):
+    return x[::-1].title()
+#Help:
+    #[::-1] reverses the String
+    #.title capitalises the first letter of each word in the string
+
+string_reversed = "tcerroc won si gnirts siht"
+
+print(func(string_reversed)) #calls the method and gives is the variable to reverse
